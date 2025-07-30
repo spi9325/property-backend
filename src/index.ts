9e737993db03fd0719d800d9121b22d2bbe4d449
spi9325 <sanketinamdar72@gmail.com>
2025-07-31 01:43:36 +0530

    test

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,10 @@ import { useSaveProperty } from "./routes/useSaveProperty";
 import { useGetProperty } from "./routes/useGetProperty";
 dotenv.config();
 const app = express()
-app.use(cors());
+app.use(cors({
+  origin: ['https://property-dashboard-roan.vercel.app'], 
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/add",useSaveProperty)
