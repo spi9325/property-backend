44ba3192b3c6edc1d01582c7a74b1cc6ede7777a
spi9325 <sanketinamdar72@gmail.com>
2025-08-02 18:47:58 +0530

    add template route assignment2

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,18 @@ import cors from "cors"
 import job from "./config/cron"
 import { useSaveProperty } from "./routes/useSaveProperty";
 import { useGetProperty } from "./routes/useGetProperty";
+import { useAddTemplate } from "./routes/useAddTemplate";
 dotenv.config();
 const app = express();
 app.use(cors({
-  origin: ['https://property-dashboard-roan.vercel.app'], 
+  origin: ['https://property-dashboard-roan.vercel.app','http://localhost:3000'], 
 }));
 if(process.env.NODE_ENV == "production") job.start();
 app.use(express.json());
 
 app.use("/add",useSaveProperty)
 app.use("/getall",useGetProperty)
+app.use("/addtemplate",useAddTemplate)
 
 
 app.listen(process.env.PORT || 8080,()=>console.log("server running on port",process.env.PORT))
